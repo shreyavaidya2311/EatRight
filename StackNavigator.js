c6ee019ab3970b2c701c7ec5f4b03ae183d7e624
Shreya Vaidya <shreyakvaidya@gmail.com>
2022-01-29 19:30:56 +0530

    feat: completed dish wise nutrition split up

--- StackNavigator.js
+++ StackNavigator.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
 import useAuth from "./hooks/useAuth";
 import Tabs from "./navigation/tabs";
+import { NutritionContent } from "./screens";
 const Stack = createNativeStackNavigator();
 const StackNavigator = () => {
   const { user } = useAuth();
@@ -13,7 +14,10 @@ const StackNavigator = () => {
       }}
     >
       {user ? (
-        <Stack.Screen name="Home" component={Tabs} />
+        <>
+          <Stack.Screen name="Home" component={Tabs} />
+          <Stack.Screen name="Nutrition" component={NutritionContent} />
+        </>
       ) : (
         <Stack.Screen name="Login" component={LoginScreen} />
       )}
