76be79222c454ac72802a29e1676f99e61ea6589
Kartik <kartikmandhan134@gmail.com>
2022-01-30 01:23:10 +0530

    feat:Login Screen UI

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,20 +1,64 @@
-import { Button, StyleSheet, Text, SafeAreaView } from "react-native";
+import { StyleSheet, Text, SafeAreaView, View, Image } from "react-native";
+
 import React from "react";
 import useAuth from "../hooks/useAuth";
-import { COLORS } from "../constants";
 import Header from "../components/Header";
+import { Button } from "react-native-paper";
 
+import { icons, COLORS, FONTS } from "../constants";
+import { loginImage } from "../constants/images";
 const LoginScreen = () => {
   const { signInWithGoogle } = useAuth();
   return (
     <SafeAreaView style={styles.container}>
       <Header />
-      <Text>Login to the app</Text>
-      <Button
-        title="Sign in with Google"
-        color={COLORS.primary}
-        onPress={signInWithGoogle}
-      />
+      <View style={{ flex: 1 }}>
+        <View>
+          <Image
+            source={loginImage}
+            style={{
+              width: "80%",
+              height: "70%",
+              alignSelf: "center",
+            }}
+          />
+          <Text
+            style={{
+              color: "white",
+              fontSize: 24,
+              justifyContent: "center",
+              alignSelf: "center",
+              marginBottom: 10,
+            }}
+          >
+            Eat Right To Weigh Right
+          </Text>
+        </View>
+        <View style={{ margin: 20 }}>
+          <Text
+            style={{
+              color: "white",
+              fontSize: 24,
+              justifyContent: "center",
+              alignSelf: "center",
+              marginBottom: 10,
+            }}
+          >
+            Login to the app
+          </Text>
+          <Button
+            icon="google"
+            mode="contained"
+            style={{
+              width: "70%",
+              alignSelf: "center",
+            }}
+            onPress={signInWithGoogle}
+          >
+            Sign In with Google
+          </Button>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
