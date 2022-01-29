031daf7acc53ecec048fdd8126a0cb3ab5d5ae8d
Sameer Kavthekaar <kavthekarsam@gmail.com>
2022-01-29 19:17:06 +0530

    update: Finished backend for goals

--- backend/routes/goals.js
+++ backend/routes/goals.js
@@ -29,4 +29,23 @@ router.post("/add-goal", async (req, res) => {
   return res.status(200).send({ msg: obj });
 });
 
+router.post("/delete-goal", async (req, res) => {
+  let { email, goal_id } = req.body;
+
+  if (!goal_id || !email)
+    return res.status(401).send({ msg: "Incorrect parameters" });
+
+  try {
+    let obj = await Goal.findByIdAndRemove({ _id: goal_id });
+    let user = await User.findOneAndUpdate(
+      { email },
+      { $pull: { goals: goal_id } }
+    );
+  } catch (err) {
+    return res.status(400).send({ msg: err });
+  }
+
+  return res.status(200).send({ msg: "Success" });
+});
+
 module.exports = router;
